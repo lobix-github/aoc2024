dc49368ebe2f8d883b3c11f89d01955636c09a4e
Łukasz Łobacz <lulo@demant.com>
2024-12-02 10:42:00 +0100

    day 2 part 2

--- aoc/d02.cs
+++ aoc/d02.cs
@@ -10,18 +10,36 @@
             list.Add(line.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => x.ToInt32()).ToList());
         }
 
-        var result1 = list.Where(x =>
-        {
-            if (x[0] == x[1]) return false;
-
-            var set = new HashSet<int>(x);
-			if (x[0] < x[1]) x.Sort(); else x.OrderDescending();
-            if (!x.SequenceEqual(set)) return false;
-
-            var safe = true;
-			x.Skip(1).Select((i, idx) => safe &= Math.Abs(x[idx] - i) <= 3).ToArray();
-            return safe;
-        }).Count();
+        var result1 = list.Where(x => test(x.ToList())).Count();
         Console.WriteLine(result1);
-    }
+
+        var unsafes = list.Where(x => !test(x.ToList())).ToList();
+        var @fixed = unsafes.Where(x =>
+        {
+            for (int i = 0; i < x.Count; i++)
+            {
+                var candidate = x.ToList();
+                candidate.RemoveAt(i);
+
+                if (test(candidate.ToList())) return true;
+            }
+            return false;
+        }).Count();
+
+        var result2 = result1 + @fixed;
+        Console.WriteLine(result2);
+	}
+
+	bool test(List<int> x)
+    {
+		if (x[0] == x[1]) return false;
+
+		var set = new HashSet<int>(x);
+		if (x[0] < x[1]) x.Sort(); else x = x.OrderDescending().ToList();
+		if (!x.SequenceEqual(set)) return false;
+
+		var safe = true;
+		x.Skip(1).Select((i, idx) => safe &= Math.Abs(x[idx] - i) <= 3).ToArray();
+		return safe;
+	}
 }
