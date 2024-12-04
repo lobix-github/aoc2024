bb249f14fa819588f420764faeb1f21618cb0acd
lobix <lobix@LOBIX-PC>
2024-12-04 17:10:34 +0100

    day 4 part 2

--- aoc/d04.cs
+++ aoc/d04.cs
@@ -61,7 +61,40 @@ class d04 : baseD
 		}
 
 		Console.WriteLine(sum);
-	}
+
+		//part 2
+		sum = 0;
+		var l = new List<char[]>();
+		foreach (var line in lines) l.Add(line.ToCharArray());
+		for (int y = 1; y < lines.Count - 1; y++)
+		{
+			for (int x = 1; x < lines[y].Length - 1; x++)
+			{
+				sum += checkXMAS(x, y);
+			}
+		}
+
+        Console.WriteLine(sum);
+
+        int checkXMAS(int x, int y)
+		{
+			if (l[y][x] == 'A')
+			{
+				if (axis1_1(x, y) && (axis2_1(x, y) || axis2_2(x, y))) return 1;
+				if (axis1_2(x, y) && (axis2_1(x, y) || axis2_2(x, y))) return 1;
+				
+				if (axis2_1(x, y) && (axis1_1(x, y) || axis1_2(x, y))) return 1;
+				if (axis2_2(x, y) && (axis1_1(x, y) || axis1_2(x, y))) return 1;
+            }
+
+			bool axis1_1(int x, int y) => l[y - 1][x - 1] == 'M' && l[y + 1][x + 1] == 'S';
+			bool axis1_2(int x, int y) => l[y - 1][x - 1] == 'S' && l[y + 1][x + 1] == 'M';
+            bool axis2_1(int x, int y) => l[y - 1][x + 1] == 'S' && l[y + 1][x - 1] == 'M';
+			bool axis2_2(int x, int y) => l[y - 1][x + 1] == 'M' && l[y + 1][x - 1] == 'S';
+
+            return 0;
+		}
+    }
 
 	int getAmount(string text) => AllIndexesOf(text, "XMAS").Length;
 }
