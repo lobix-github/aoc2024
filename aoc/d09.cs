0aa6dd58e50af5b6cb525d0e97482f8b3e24e1f2
Łukasz Łobacz <lulo@demant.com>
2024-12-09 13:14:34 +0100

    day 9 part 2

--- aoc/d09.cs
+++ aoc/d09.cs
@@ -1,12 +1,12 @@
-﻿using System.Numerics;
-
-class d09
+﻿class d09
 {
     public void Run()
     {
         var input = File.ReadAllText(@"..\..\..\inputs\09.txt");
         var list = new List<infoD09>();
         var empties = new List<int>();
+        var blocks = new List<infoD09>();
+        var emptyBlocks = new List<infoD09>();
 
         int id = 0;
         int idx = 0;
@@ -17,12 +17,23 @@ class d09
             {
 				if (i % 2 == 0)
 				{
-                    list.Add(new infoD09(id, idx));
+					var info = new infoD09(id, idx, c - '0', false);
+					list.Add(info);
+					if (j == 0)
+					{
+						blocks.Add(info);
+					}
 				}
-                else
+				else
                 {
-                    list.Add(new infoD09(-1, idx));
+					var info = new infoD09(-1, idx, c - '0', false);
+
+					list.Add(info);
                     empties.Add(idx);
+					if (j == 0)
+					{
+						emptyBlocks.Add(info);
+					}
 				}
                 idx++;
 			}
@@ -32,31 +43,66 @@ class d09
 			}
 		}
 
-        while (empties.Any())
-        {
-            if (list.Skip(empties.First()).Any(x => x.id != -1))
-            {
-				var last = list.Last(x => x.id != -1);
-				var lastIdx = list.IndexOf(last);
-				list[empties.First()] = last;
-				list[lastIdx] = last with { id = -1 };
-				empties.RemoveAt(0);
-			}
-            else
-            {
-                break;
-            }
-		}
+		part1();
+		part2();
 
-        long sum = 0;
+		long sum = 0;
         for (int i = 0; i < list.Count; i++)
         {
-            if (list[i].id == -1) break;
-            sum += i * list[i].id;
-        }
+			var val = list[i].id;
+			if (val != -1)
+            {
+				sum += i * val;
+			}
+		}
 
 		Console.WriteLine(sum);
+
+        void part1()
+        {
+			while (empties.Any())
+			{
+				if (list.Skip(empties.First()).Any(x => x.id != -1))
+				{
+					var last = list.Last(x => x.id != -1);
+					var lastIdx = list.IndexOf(last);
+					list[empties.First()] = last;
+					list[lastIdx] = last with { id = -1 };
+					empties.RemoveAt(0);
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+
+		void part2()
+		{
+			for (int i = blocks.Count - 1; i >= 0; i--)
+			{
+				var block = blocks[i];
+				for (int j = 0; j < emptyBlocks.Count; j++)
+				{
+					var emptyBlock = emptyBlocks[j];
+					if (emptyBlock.len >= block.len)
+					{
+						if (emptyBlock.idx < block.idx)
+						{
+							for (int idx = 0; idx < block.len; idx++)
+							{
+								list[emptyBlock.idx + idx] = block;
+								list[block.idx + idx] = block with { id = -1 };
+							}
+
+							emptyBlocks[j] = emptyBlock with { idx = emptyBlock.idx + block.len, len = emptyBlock.len - block.len };
+							break;
+						}
+					}
+				}
+			}
+		}
 	}
 
-    record infoD09(int id, int idx);
+	record infoD09(int id, int idx, int len, bool moved);
 }
